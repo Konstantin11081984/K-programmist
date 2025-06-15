1c5ef44f6c9b31f7a9dcd011cdd0808f9bdd99fe
Konstantin11081984 <varenik.konstantin@gmail.com>
2025-06-15 12:07:35 +0700

    Update 06.py

--- 06.py
+++ 06.py
@@ -1,19 +1,17 @@
 import telebot
 
-API_TOKEN = '7757258327:AAEfSZet5H3k6WJj9iY8wbqepPOUVyj003s'
+API_TOKEN = 'YOUR_BOT_TOKEN_HERE'  # вставь свой токен
 
 bot = telebot.TeleBot(API_TOKEN)
 
-# Обработчик команды /start и /help
+# Обработчик команд /start и /help
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Привет! Я телеграм-бот. Чем могу помочь?")
 
-# Ответ на любое другое сообщение
+# Ответ на любое сообщение
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
 
-# Запуск бота
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+# Запуск
